2055d30c769821f5a61778f4d04f4d08878983f9
markajKING <mark.hobbel@live.nl>
2022-06-01 12:08:59 +0200

    movement improvent

--- src/brandaan.ts
+++ src/brandaan.ts
@@ -7,6 +7,7 @@ export class Brandaan extends PIXI.AnimatedSprite {
     private xSpeed: number = 0
     private ySpeed: number = 0
     private frames: PIXI.Texture[][] = []
+    private previousFrame: number = -1
 
     constructor(game: Game, textures: PIXI.Texture[][], x: number, y: number) {
         super(textures[0])
@@ -77,9 +78,21 @@ export class Brandaan extends PIXI.AnimatedSprite {
     }
 
     private setFrames(frame: number) {
-        this.textures = this.frames[frame]
-        this.loop = true
-        this.play()
+        console.log(this.previousFrame)
+        if (this.previousFrame != frame) {
+
+            console.log("set frames");
+
+            this.textures = this.frames[frame]
+
+            this.loop = true
+
+            this.play()
+
+            this.previousFrame = frame
+
+        }
+
     }
 
     public update(delta: number): void {
